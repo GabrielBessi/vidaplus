e0ad2622c0345a352cb9e418306fad9ebaebda5c
Gabriel Bessi <tecbessi@gmail.com>
2025-12-03 20:08:17 -0300

    Adicionando migrate automatico

--- run.py
+++ run.py
@@ -4,34 +4,3 @@ app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-# Amanha
-
-# Limpar importações não utilizadas - ok
-# Padronizar nomes para portugues -ok
-
-#Adicionar auditoria em mais lugares - ok
-
-# Apagar tudo do banco e criar tudo de novo - ok
-
-# Testar 
-#     prontuario
-#     telemedicina
-#     auditoria
-
-# Criar alguns testes unitarios
-
-# estando tudo ok, subir aplicação e banco para algum lugar que seja possível acessar de qualquer lugar
-
-#criar documentação da API
-
-# montar documento final
-
-# Login - ok
-# administracao -ok
-# usuarios -ok
-# paciente ok
-# consulta - ok
-# prontuario - ok
-# Profissionais - ok
-# Telemedicina

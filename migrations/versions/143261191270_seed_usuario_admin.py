b47eb3d8d9a501117b8f8cbf99c751c2effdc38c
Gabriel Bessi <tecbessi@gmail.com>
2025-12-03 21:29:55 -0300

    ta quase la

--- migrations/versions/143261191270_seed_usuario_admin.py
+++ migrations/versions/143261191270_seed_usuario_admin.py
@@ -19,11 +19,11 @@ depends_on = None
 def upgrade():
     op.execute("""
         INSERT INTO usuarios (id, nome, email, senha, perfil)
-        VALUES (1, 'Administrador', 'adm1@gmail.com', 'scrypt:32768:8:1$40UvnIIG593r6Fbp$e1b1f3283bdee21e9e9e26dbd37792b1fda5785c274ac85c0a082dcec952ac8bcafbb935245826313d1f48ae2c8317480d6f39ef039d63ef5b0e1bcd6916ec55', 'administrador')
+        VALUES (2, 'Administrador', 'adm1@gmail.com', 'scrypt:32768:8:1$40UvnIIG593r6Fbp$e1b1f3283bdee21e9e9e26dbd37792b1fda5785c274ac85c0a082dcec952ac8bcafbb935245826313d1f48ae2c8317480d6f39ef039d63ef5b0e1bcd6916ec55', 'administrador')
     """)
     op.execute("""
         INSERT INTO administradores (id, cpf, data_nascimento, endereco, telefone)
-        VALUES (1, '12345678910', '1999-01-01', 'Rua Brasil, 6', '11 99999999')
+        VALUES (2, '12345678910', '1999-01-01', 'Rua Brasil, 6', '11 99999999')
     """)
 
 def downgrade():
